ae958855c536551784d1f418d6f5701afd14c187
germansosagoy <germanesosagoy@gmail.com>
2024-06-10 20:16:41 -0300

    chore: classes in tailwind.config.js & ts.configjson, vite.config.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,33 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:{
+          light: '#FFFFFF',
+          DEFAULT: '#3490DC',
+          dark: '#1D3A5F',
+        },
+        secondary:{
+          light: '#FDEDEC',
+          DEFAULT: '#E3342F',
+          dark: '#6B0F1A',
+        },
+        neutral: {
+          light: '#F5F5F5',
+          DEFAULT: '#3D4852',
+          dark: '#22292F',
+        },
+        dark:{
+          background: '#090D1F',
+          text: '#FFFFFF',
+          primary: '#2563EB',
+          secondary: '#DC2626',
+        }
+      }
+    },
   },
   plugins: [],
 }
